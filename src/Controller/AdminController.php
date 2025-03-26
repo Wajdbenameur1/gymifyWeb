190ac6910c66f9754e60addcfc8f0e39a6604492
wajd <97837888+Wajdbenameur1@users.noreply.github.com>
2025-03-26 18:14:40 +0100

    wajd mercredi 26

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+final class AdminController extends AbstractController
+{
+    #[Route('/wajd', name: 'app_admin')]
+    public function index(): Response
+    {
+      $stats = [
+        'visitors' => 1294,
+        'subscribers' => 1303,
+        'sales' => 1345,
+        'orders' => 576
+    ];
+
+    return $this->render('index.html.twig', [
+        'stats' => $stats,
+        'page_title' => 'Tableau de bord'
+    ]);
+    }
+    #[Route('/home', name: 'app_home')]
+      public function home()
+      {
+    // Your controller code
+       }
+       #[Route('/dashboard', name:'app_dashboard')]
+      public function dashboard()
+      {
+    
+       }
+       #[Route('/profile', name:'app_profile')]
+      public function profile()
+      {
+    
+       }
+       #[Route('/login', name:'app_login')]
+      public function login()
+      {
+    
+       }
+       #[Route('/about', name:'app_about')]
+      public function about()
+      {
+    
+       }
+       #[Route('/help', name:'app_help')]
+      public function help()
+      {
+    
+       }
+       #[Route('/term', name:'app_terms')]
+       public function term()
+       {
+     
+        }
+
+
+       
+
+    
+    
+}
