7753b3333af23b533c6791d3da4b36effb05d783
GhabiJihen <GhabiJihen@users.noreply.github.com>
2025-04-05 13:46:42 +0100

    Update Sportif.php
    jihen

--- src/Entity/Sportif.php
+++ src/Entity/Sportif.php
@@ -1,14 +1,14 @@
 <?php
+
 namespace App\Entity;
 
 use App\Enum\Role;
+use App\Repository\SportifRepository;
 use Doctrine\ORM\Mapping as ORM;
-
-
+#[ORM\Entity]
+#[ORM\Entity(repositoryClass: SportifRepository::class)]
 class Sportif extends User
 {
-  
-
     public function __construct()
     {
         parent::__construct();
