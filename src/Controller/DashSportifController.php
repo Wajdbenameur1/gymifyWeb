42f1a87b077833b8249de05cb14e7381e4b5b577
Ranym Mejri <124840327+ranymmjr@users.noreply.github.com>
2025-04-08 12:19:01 +0100

    third

--- src/Controller/DashSportifController.php
+++ src/Controller/DashSportifController.php
@@ -6,38 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
-
-
-
-
-final class DashSportifController extends AbstractController
-{
-    #[Route('/dashsportif', name: 'app_dashsportif')]
+final class DashSportifController extends AbstractController{
+    #[Route('/dash/sportif', name: 'app_dash_sportif')]
     public function index(): Response
     {
-      $stats = [
-        'visitors' => 1294,
-        'subscribers' => 1303,
-        'sales' => 1345,
-        'orders' => 576
-    ];
-
-    return $this->render('dashsportif/index.html.twig', [
-        'stats' => $stats,
-        'page_title' => 'Tableau de bord'
-    ]);
+        return $this->render('dash_sportif/index.html.twig', [
+            'controller_name' => 'DashSportifController',
+        ]);
     }
-   
-       
-       
-      
-        
-  }
-
-
-       
-
-    
-    
-
+}
