e9e248250af1c8e61aac24dc232fbae467d461ca
Ranym Mejri <124840327+ranymmjr@users.noreply.github.com>
2025-04-15 18:37:52 +0100

    v16

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -1,31 +1,51 @@
 <?php
+
 namespace App\Form;
 
 use App\Entity\Post;
-use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\UrlType; // <-- AJOUTE CETTE LIGNE !!
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('title', TextType::class, [
-            'label' => 'Titre du post',
-        ])
-        ->add('content', TextType::class, [
-            'label' => 'Contenu du post',
-        ])
-        ->add('image_url', UrlType::class, [
-            'label' => 'Image (URL)',
-            'required' => false,
-        
+            ->add('title', TextType::class, [
+                'label' => 'Titre du post',
+                'attr' => [
+                    'required'                           => 'required',
+                    'data-parsley-minlength'             => '3',  // vous pouvez mettre 5 ici si nécessaire
+                    'data-parsley-minlength-message'     => 'Le titre doit contenir au moins 3 caractères.',
+                    'data-parsley-maxlength'             => '100',
+                    'data-parsley-maxlength-message'     => 'Le titre ne peut pas dépasser 100 caractères.',
+                    'data-parsley-pattern'               => '^(?!.*\b(spam|arnaque|insulte)\b).*$',
+                    'data-parsley-trigger'               => 'keyup'
+                ],
+            ])
+            ->add('content', TextType::class, [
+                'label' => 'Contenu du post',
+                'attr' => [
+                    'required'                           => 'required',
+                    'data-parsley-minlength'             => '10',  // vous pouvez mettre 5 ici si nécessaire
+                    'data-parsley-minlength-message'     => 'Le contenu doit contenir au moins 10 caractères.',
+                    'data-parsley-maxlength'             => '1000',
+                    'data-parsley-maxlength-message'     => 'Le contenu ne peut pas dépasser 1000 caractères.',
+                    'data-parsley-pattern'               => '^(?!.*\b(spam|arnaque|insulte)\b).*$',
+                    'data-parsley-trigger'               => 'keyup'
+                ],
+            ])
+            ->add('image_url', UrlType::class, [
+                'label'    => 'Image (URL)',
+                'required' => false,
+                'attr'     => [
+                    'data-parsley-type'           => 'url',
+                    'data-parsley-pattern'        => '/\.(jpg|jpeg|png|gif)$/i',
+                    'data-parsley-trigger'        => 'keyup'
+                ],
             ]);
     }
 
